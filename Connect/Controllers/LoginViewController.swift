e176559a7fa2af4dce34686e7fb7992c88cd4dc5
Brian Zhu <bzhu@parktudor.org>
2020-07-29 17:14:40 -0400

    Directory Organization

---
+++ Connect/Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Connect
+//
+//  Created by Brian Zhu on 7/29/20.
+//  Copyright © 2020 Brian Zhu. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
